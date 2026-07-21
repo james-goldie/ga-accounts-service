ed3fde06644b54331ee16a9ae474f4eaa246f0a3
james <t2_177@outlook.com>
2026-07-21 10:21:39 +0100

    update app

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import express from "express";
 
-const port = process.env.PORT;
+const port = 3000;
+const app = express();
 
-console.log(port);
+app.use(express.json());
+
+async function startServer() {
+  try {
+    app.listen(port, () => console.log(`🤖 Listening on Port: ${port}`));
+  } catch (err) {
+    console.log("🤖 Oh no something went wrong", err);
+  }
+}
+
+startServer();
